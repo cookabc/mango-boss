d33fddf2cb9479a1ecacc328940268eddc987830
xugang <xugang.song@advance.ai>
2020-04-12 19:23:39 +0800

    Add login process

--- mango-admin/src/main/java/com/example/mango/admin/controller/SysDictController.java
+++ mango-admin/src/main/java/com/example/mango/admin/controller/SysDictController.java
@@ -4,6 +4,7 @@ import com.example.mango.admin.model.SysDict;
 import com.example.mango.admin.service.SysDictService;
 import com.example.mango.core.http.HttpResult;
 import com.example.mango.core.page.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,21 +17,25 @@ public class SysDictController {
     @Resource
     private SysDictService sysDictService;
 
+    @PreAuthorize("hasAuthority('sys:dict:add') AND hasAuthority('sys:dict:edit')")
     @PostMapping(value = "/save")
     public HttpResult save(@RequestBody SysDict record) {
         return HttpResult.ok(sysDictService.save(record));
     }
 
+    @PreAuthorize("hasAuthority('sys:dict:delete')")
     @PostMapping(value = "/delete")
     public HttpResult delete(@RequestBody List<SysDict> records) {
         return HttpResult.ok(sysDictService.delete(records));
     }
 
+    @PreAuthorize("hasAuthority('sys:dict:view')")
     @GetMapping(value = "/findByLabel")
     public HttpResult findAll(@RequestParam String label) {
         return HttpResult.ok(sysDictService.findByLabel(label));
     }
 
+    @PreAuthorize("hasAuthority('sys:dict:view')")
     @PostMapping(value = "/findPage")
     public HttpResult findPage(@RequestBody PageRequest pageRequest) {
         return HttpResult.ok(sysDictService.findPage(pageRequest));
