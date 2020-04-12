835b71f67ffa5f108071c43f29146ec558430a47
xugang <xugang.song@advance.ai>
2020-04-12 11:01:02 +0800

    Add sys dept

---
+++ mango-admin/src/main/java/com/example/mango/admin/controller/SysDeptController.java
@@ -0,0 +1,32 @@
+package com.example.mango.admin.controller;
+
+import com.example.mango.admin.model.SysDept;
+import com.example.mango.admin.service.SysDeptService;
+import com.example.mango.core.http.HttpResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "dept")
+public class SysDeptController {
+
+    @Resource
+    private SysDeptService sysDeptService;
+
+    @PostMapping(value = "/save")
+    public HttpResult save(@RequestBody SysDept record) {
+        return HttpResult.ok(sysDeptService.save(record));
+    }
+
+    @PostMapping(value = "/delete")
+    public HttpResult delete(@RequestBody List<SysDept> records) {
+        return HttpResult.ok(sysDeptService.delete(records));
+    }
+
+    @GetMapping(value = "/findTree")
+    public HttpResult findTree() {
+        return HttpResult.ok(sysDeptService.findTree());
+    }
+}
---
+++ mango-admin/src/main/java/com/example/mango/admin/service/SysDeptService.java
@@ -0,0 +1,17 @@
+package com.example.mango.admin.service;
+
+import com.example.mango.admin.model.SysDept;
+import com.example.mango.core.service.CrudService;
+
+import java.util.List;
+
+public interface SysDeptService extends CrudService<SysDept> {
+
+    /**
+     * 查询机构树
+     *
+     * @return 部门列表
+     */
+    List<SysDept> findTree();
+
+}
